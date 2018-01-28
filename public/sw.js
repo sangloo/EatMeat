21ee2abbe158cfe1998cb84ac5b3911bc6f9253e
khalilall <alamikhalil@hotmail.fr>
2018-01-28 19:44:57 +0100

    mobileready2
    mobileready2

--- public/sw.js
+++ public/sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-1e818e5c768881b25c33.js","634f4562e925070d9f158a694653aeee"],["/commons-6fdcceeae6a81a5398db.js","a0cee8699ca4527eea4fb4608c9a8b17"],["/index.html","085aa900a8b3416af3a05c045a8fad31"],["/manifest.json","ea110a70d06884a8b425a3d043673b30"],["/offline-plugin-app-shell-fallback/index.html","3a0cc4eaf1df02e6d82bdb896b595ac2"]];
+var precacheConfig = [["/app-1e818e5c768881b25c33.js","634f4562e925070d9f158a694653aeee"],["/commons-6fdcceeae6a81a5398db.js","a0cee8699ca4527eea4fb4608c9a8b17"],["/index.html","d490fffbdae36778aa319bdf5d6c2700"],["/manifest.json","ea110a70d06884a8b425a3d043673b30"],["/offline-plugin-app-shell-fallback/index.html","e8c6177e751ac79b91f5e721427e7ed2"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
